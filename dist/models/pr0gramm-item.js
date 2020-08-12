42a6ff03f53ff02951bd79df5171fbd861276898
akop <akop@ppi.de>
2020-08-12 18:20:31 +0200

    better gitignore

--- dist/models/pr0gramm-item.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Pr0grammItem {
-}
-exports.Pr0grammItem = Pr0grammItem;
-//# sourceMappingURL=pr0gramm-item.js.map
